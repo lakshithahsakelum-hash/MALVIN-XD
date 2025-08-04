49341567706e16b532138ccb01a54fa28feb76e3
lakshithahsakelum-hash <lakshithahsakelum@gmail.com>
2025-08-04 17:13:46 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~YmsizBJa#BosWzJ7F2PP3gfRLOK6ctBtRLBC26_O822qHqkVxVsM",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
